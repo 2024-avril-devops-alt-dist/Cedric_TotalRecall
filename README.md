8ff6e5abe8728b19cf9350fbfa573c39a49f808f
Mendoz38 <cedric@mendoz.fr>
2024-11-06 09:49:21 +0100

    simplify docker-compose.yml

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
-## Getting Started
+# API Total Recall
+
+## Description
+
+This is an API with NextJS, TypeScript on a MangoDB database managed with Prisma.
+
+## Requirement
+
+To run this API we need to have :
+* Docker installed
+
+## Run the project
+* Clone this repo
+* Run docker compose up --build -d
+* Configure Mongo container :
+    * Go to the mongo container : ```docker exec -it mongo mongosh```
+    * ```rs.initiate()```
+    * To configure the host : ```rs.reconfig({ _id: 'rs0', members: [ { _id: 0, host: 'mongo:27017' }] }, { force: true });```
+    * To show the config ```rs.status()```
+    * Exit the container ```exit```
+
+
 
 First, run the development server:
 
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,28 @@
-import { PrismaClient } from '@prisma/client';
-
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-const initialPosts = [
-  { name: 'Post 1' },
-  { name: 'Post 2' },
-  { name: 'Post 3' },
-];
+async function main() {
+  await prisma.user.createMany({
+    data: [
+      { name: "Ced3", email: "ced3@ced.fr" },
+      { name: "Ced4", email: "ced4@ced.fr" },
+    ],
+  });
 
-const seed = async () => {
-    
-  await prisma.user.createMany();
+  await prisma.comment.createMany({
+    data: [
+      { name: "Ced3", title: "Ced3" },
+      { name: "Ced4", title: "Ced4" },
+    ],
+  });
+}
 
-    await prisma.user.create({
-      data: {
-        email: 'aaa@aaa0fr',
-        name : 'aaa'
-      }
-    });
-  }
-
-seed();
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
