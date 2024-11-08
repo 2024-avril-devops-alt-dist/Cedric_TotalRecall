da53d5d09dab287e5f3695b24703563b6dc58d8a
Mendoz38 <cedric@mendoz.fr>
2024-11-08 15:27:49 +0100

    add put and delete to company name

--- app/api/company/route.ts
+++ app/api/company/route.ts
@@ -7,7 +7,8 @@ const prisma = new PrismaClient();
 /* ######## Collection variable ########## */
   const collection = "company"; 
   const response = "companies";
-  
+  const id_collection = "id_company"
+
 /*-------------------------- GET ---------------------------------*/
 export async function GET(req: NextRequest) {
   const dbCheck = checkDatabase();
@@ -40,6 +41,66 @@ export async function POST(req: NextRequest) {
   } catch (error) {
     return NextResponse.json(
       { error: `Failed to create ${collection}` },
+      { status: 500 }
+    );
+  }
+}
+
+/*-------------------------- UPDATE ---------------------------------*/
+export async function PUT(req: NextRequest) {
+  const dbCheck = checkDatabase();
+  if (dbCheck) return dbCheck;
+
+  try {
+    const body = await req.json();
+    console.log("Dans update",body )
+    const { [id_collection]: idValue, ...dataToUpdate } = body;
+console.log("----------------------id_collection :", id_collection)
+    if (!idValue) {
+      return NextResponse.json(
+        { error: `ID is required to update ${collection}` },
+        { status: 400 }
+      );
+    }
+
+    const updatedData = await prisma[collection].update({
+      where: { [id_collection]: idValue },
+      data: dataToUpdate,
+    });
+
+    return NextResponse.json({ [response]: updatedData });
+  } catch (error) {
+    return NextResponse.json(
+      { error: `Failed to update ${collection}` },
+      { status: 500 }
+    );
+  }
+}
+
+/*-------------------------- DELETE ---------------------------------*/
+export async function DELETE(req: NextRequest) {
+  console.log("Dans update")
+  const dbCheck = checkDatabase();
+  if (dbCheck) return dbCheck;
+
+  try {
+    const { [id_collection]: idValue } = await req.json();
+
+    if (!idValue) {
+      return NextResponse.json(
+        { error: `ID is required to delete ${collection}` },
+        { status: 400 }
+      );
+    }
+
+    const deletedData = await prisma[collection].delete({
+      where: { [id_collection]: idValue },
+    });
+
+    return NextResponse.json({ message: `${collection} deleted successfully`, [response]: deletedData });
+  } catch (error) {
+    return NextResponse.json(
+      { error: `Failed to delete ${collection}` },
       { status: 500 }
     );
   }
