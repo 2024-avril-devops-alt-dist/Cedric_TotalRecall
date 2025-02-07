85d6f1f74e8250d1c3f0b7467137b10be5356480
Mendoz38 <cedric@mendoz.fr>
2025-02-07 10:06:04 +0100

    Configure react-admin for users

--- app/api/v1/users/[id]/route.ts
+++ app/api/v1/users/[id]/route.ts
@@ -1,18 +1,11 @@
-// app/api/v?/compagnies/route.ts
+// app/api/v?/user/[id]/route.ts
 
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import { checkDatabase } from "../../../../utils/connectDB";
 import { handleError } from "../../../../utils/response"
-//import { protectRoute } from "@/lib/auth"; 
 
 const prisma = new PrismaClient();
-   /* TO PROTECT ROUTE ADD
-  const token = await protectRoute(req);
-  if (token instanceof Response) { return token;  }
-  */
- 
-/* ######## Collection variable ########## */
   const collection = "user"; 
   const response = "users";
   const id_collection = "id_user"
@@ -32,4 +25,21 @@ export async function GET(req: NextRequest) {
   } catch (error) {
       return handleError(error, collection);
     }
+}
+
+/*-------------------------- GET by Email ---------------------------------*/
+export async function GET_BY_EMAIL(req: NextRequest) {
+  const email = req.nextUrl.searchParams.get("email");
+  const dbCheck = checkDatabase();
+
+  if (dbCheck) return dbCheck;
+
+  try {
+    const data = await prisma[collection].findUnique({
+      where: { email: email },
+    });
+    return NextResponse.json({ [response]: data ?? [] });
+  } catch (error) {
+    return handleError(error, collection);
+  }
 }
